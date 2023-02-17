ade4a8f752c40cc0c5ef9076758f4292cce46754
DEVFATH <ggje33@gmail.com>
2023-02-17 16:30:32 +0900

    initial commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,9 +5,9 @@ const config: CapacitorConfig = {
   appName: 'next-ev-infra-clone',
   webDir: 'out',
   bundledWebRuntime: false,
-  server: {
-    url: 'http://10.0.15.53:3000'
-  }
+  // server: {
+  //   url: 'http://localhost:3000'
+  // }
 };
 
 export default config;
